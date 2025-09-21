19bc412fd8a27f7158df9afc8510fa0f82848b52
Shabri <Shabri@RockyShabri>
2025-09-21 14:56:16 +0530

    Fb is updated

--- src/test/java/basics/LaunchBrowser.java
+++ src/test/java/basics/LaunchBrowser.java
@@ -3,15 +3,18 @@ package basics;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
-public class LaunchBrowser{
-	public static void main(String [] args) {
-	
+
+
+class LaunchBrowser {
+@Test
+	public void launchBrowser() {
+
 		WebDriver driver = new ChromeDriver();
-		
 		driver.get("https://www.facebook.com/");
-		Reporter.log(driver.getTitle(),true);
+		Reporter.log(driver.getTitle(), true);
 		driver.quit();
-		
+
 	}
 }
