eea4ce83a483dc58f63f4415cb7dfae217b9de8b
Shabri <Shabri@RockyShabri>
2025-09-21 14:23:51 +0530

    Inital commit

---
+++ src/test/java/basics/demo.java
@@ -0,0 +1,14 @@
+package basics;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo {
+@Test
+	public void test() {
+		
+		Reporter.log("Hello", true);
+	}
+	
+	
+}
